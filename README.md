25729c1cc4a981384a3e88b9db7ef60b52c1d592
Tom Guinther <tguinther@overtone.studio>
2018-09-08 10:34:14 -0400

    updated. README still WIP

--- README.md
+++ README.md
@@ -13,5 +13,65 @@ $ go get github.com/gotomgo/go-promises
 2. Import it in your code:
 
 ```go
-import promises "github.com/gotomgo/promises"
+import promises "github.com/gotomgo/go-promises"
+```
+
+3. Review the provided examples
+
+The examples can be found in the examples folder or at https://github.com/gotomgo/go-promises/tree/master/examples
+
+## Basic Usage
+There are two primary interfaces exposed by the promises package:
+* Controller
+* Promise
+
+Controller is a super-set of Promise and is used by code that controls delivery of the promise, while
+Promise is intended for consumers that react to delivery of the promise.
+
+### Creating a Promise
+The function, NewPromise, returns a Controller (which again, is a Promise) but methods that return an instance of a promise typically return Promise.
+
+Here is a basic template for a method that creates and returns a Promise:
+
+```go
+func myFunction() promises.Promise {
+	// NewPromise returns Controller, which is a superset of Promise
+	p := promises.NewPromise()
+
+  // perform work here. Synchronously or Asynchronously
+
+	return p
+}
+```
+
+### Using a Promise
+
+Consumers of a Promise are primarily interested in the delivery of a promise, and if it was successfully delivered, processing the result. They are also typically interested in errors as well.
+
+```go
+func consumer() {
+  myFunction().Success(func(result interface{}) {
+    // do something with the result
+  }).Catch(func (err error) {
+    fmt.Println("Error from myFunction: ",err)
+  })
+}
+```
+
+In other cases you may want to use Always to perform an action or process the result.
+
+```go
+func consumer() {
+  myFunction().Always(func(p promises.Controller) {
+    // do something now that the promise is delivered
+
+    someFunctionUnrelatedToResult()
+
+    // or process the result like follows:
+    if p.Succeeded() {
+      result := p.Result()
+    } else {
+      fmt.Println("Error from myFunction: ",p.Error())
+    }
+  })
 ```
--- examples/basic/main.go
+++ examples/basic/main.go
@@ -4,41 +4,76 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 	"sync"
 
 	promises "github.com/gotomgo/go-promises"
 )
 
-func download(uri string) promises.Promise {
+// doDownload does the actual download, synch or asynch
+//
+//  Notes
+//    The function requires a promises.Controller as it will deliver the
+//    promise, whereas a normal consumer of a promise only needs
+//    and instance of promises.Promise
+//
+//    The function returns the Promise *only* to make the code
+//    a little cleaner (don't need seperate line with 'return'.)
+//    We could re-structure the code to avoid this, but its an example
+func doDownload(uri string, p promises.Controller) promises.Promise {
+	client := &http.Client{}
+	r, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		// fail the promise with the error from the http request
+		return p.Fail(err)
+	}
+
+	resp, err := client.Do(r)
+	if err != nil {
+		// fail the promise with the error from the http client
+		return p.Fail(err)
+	}
+
+	// keep it simple, and only succedd on 200
+	if resp.StatusCode != http.StatusOK {
+		return p.Fail(fmt.Errorf("HTTP STATUS (%d): %s", resp.StatusCode, resp.Status))
+	}
+
+	defer resp.Body.Close()
+
+	// JIC U R Curious
+	// fmt.Println("Download content length = ", resp.ContentLength)
+
+	if bodyBytes, err := ioutil.ReadAll(resp.Body); err == nil {
+		return p.SucceedWithResult(bodyBytes)
+	} else {
+		return p.Fail(err)
+	}
+}
+
+// synchDownload downloads a resource and returns a promise
+// for delivery of the results. Because the download is synchronous,
+// the promise is delivered prior to the function returning
+func synchDownload(uri string) promises.Promise {
+	// NewPromise returns Controller, which is a superset of Promise
+	p := promises.NewPromise()
+
+	doDownload(uri, p)
+
+	return p
+}
+
+// aynchDownload starts a GO routine to download the file and returns a promise
+// for delivery of the results
+func asynchDownload(uri string) promises.Promise {
 	// NewPromise returns Controller, which is a superset of Promise
 	p := promises.NewPromise()
 
 	// do the download asynchronously
-	go func(p promises.Controller, uri string) {
-		client := &http.Client{}
-		r, _ := http.NewRequest("GET", uri, nil)
-
-		resp, err := client.Do(r)
-		if err != nil {
-			// fail the promise with the error from the http client
-			p.Fail(err)
-			return
-		}
-
-		// keep it simple, and only succedd on 200
-		if resp.StatusCode != http.StatusOK {
-			p.Fail(fmt.Errorf("HTTP STATUS (%d): %s", resp.StatusCode, resp.Status))
-			return
-		}
-
-		defer resp.Body.Close()
-
-		if bodyBytes, err := ioutil.ReadAll(resp.Body); err == nil {
-			p.SucceedWithResult(bodyBytes)
-		} else {
-			p.Fail(err)
-		}
-	}(p, uri)
+	go func(uri string, p promises.Controller) {
+		doDownload(uri, p)
+	}(uri, p)
 
 	return p
 }
@@ -50,11 +85,25 @@ func main() {
 	// keep-alive until the download attempt completes
 	wg.Add(1)
 
-	download("https://github.com/gotomgo/go-promises/examples/testdata/image1.jpg").
-		Success(func(result interface{}) {
-			bodyBytes := result.([]byte)
-			fmt.Printf("Downloaded %d bytes\n", len(bodyBytes))
-		}).Catch(func(err error) {
+	uri := "https://github.com/gotomgo/go-promises/tree/master/examples/testdata/image1.jpg"
+
+	// use a function pointer so we can switch between asynch (default) or
+	// synch download
+	var download func(string) promises.Promise
+
+	// default is asynch
+	download = asynchDownload
+
+	// allow a command-line arg to tell us to do the download synchronously
+	if len(os.Args) > 1 && strings.ToLower(os.Args[1]) == "synch" {
+		download = synchDownload
+	}
+
+	// do the download and process the results
+	download(uri).Success(func(result interface{}) {
+		bodyBytes := result.([]byte)
+		fmt.Printf("Downloaded %d bytes\n", len(bodyBytes))
+	}).Catch(func(err error) {
 		fmt.Println("Error downloading file: ", err)
 	}).Always(func(p promises.Controller) {
 		// tell the main thread the download attempt completed
